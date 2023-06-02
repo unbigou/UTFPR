e06e0f6955d94389c3670e65601b7ae83956924d
unbigou <cookiesasas@gmail.com>
2023-06-02 04:00:30 -0300

    só falta os sinais

--- Sistemas Operacionais/Trabalho 1 - Sincronização e Concorência/main.cpp
+++ Sistemas Operacionais/Trabalho 1 - Sincronização e Concorência/main.cpp
@@ -6,39 +6,31 @@
 #include <stdio.h>
 #include <random>
 
-using namespace std;
-
-const int QTT_ALUNOS = 3;
-const int QTT_MONITORES = 1;
-const int TAM_DOS_GRUPOS = 2;
+const int QTT_ALUNOS = 11; // <- Quantidade de alunos 
+const int QTT_MONITORES = 3; // <- Quantidade de monitores 
+const int TAM_DOS_GRUPOS = 3; // <- Tamanho de cada grupo
 
 sem_t sala_vazia;
 sem_t vagas_na_sala;
 sem_t monitores_em_sala;
 
+
 void* Alunos_de_SO(void* id)
 {
     int id_aluno = *(int*) id;
 
-    // Baguio de sinal aqui.......
+	sem_wait(&vagas_na_sala);	//Esperar por vaga na sala, ou subtrai caso entre.
 
-    while(1)
+	printf("[Aluno %d entrou na sala]\n", id_aluno);
+	while(1) 
     {
-		sem_wait(&vagas_na_sala);	//Aluno espera por vaga na sala, ou subtrai a quandidade de vagas caso entre.
-		
-		printf("[Aluno %d entrou na sala]\n", id_aluno);
-		while(1)
-		{
-			printf("[Aluno %d está estudando]\n", id_aluno);	 sleep(3);
+		printf("[Aluno %d está estudando]\n", id_aluno);	sleep(3);
 
-			if(rand() % 5 == 1)	//Chance do aluno sair da sala
-			{   
-				sem_post(&vagas_na_sala);	printf("[Aluno %d saiu da sala]\n", id_aluno);
-				break;
-			}
+		if(rand() % 4 == 1)	//Chance do aluno sair da sala
+		{   
+			printf("[Aluno %d saiu da sala]\n", id_aluno);	sem_post(&vagas_na_sala);   break;
 		}
-		break;
-    }
+	}
     pthread_exit(NULL);
 }
 
@@ -48,34 +40,32 @@ void* Estudantes_Monitores(void* id)
     int aux_mtr, aux_aln;
     int id_monitor = *(int*) id;
     
-    // Aqui é pra ter o sinal......
-
     sem_post(&monitores_em_sala);   printf("[Monitor %d entrou na sala]\n", id_monitor);
 
-	for(int i = 0; i < TAM_DOS_GRUPOS; i++) 
+	for(int k = 0; k < TAM_DOS_GRUPOS; k++)
+    {
 		sem_post(&vagas_na_sala);
+    }
 
     while(1)
     {
-        //Monitor supervisionando alunos...
-        printf("[Monitor %d está supervisionando os alunos]\n", id_monitor); sleep(3);
+        printf("[Monitor %d está supervisionando os alunos]\n", id_monitor);    sleep(3);
 
+        //Monitor tenta sair aleatóriamente ou caso não haja mais alunos na sala
         if(rand() % 4 == 1 || aux_aln == TAM_DOS_GRUPOS * QTT_MONITORES)
         {
             sem_getvalue(&vagas_na_sala, &aux_aln);
 
-            if(aux_aln >= TAM_DOS_GRUPOS)
+            if(aux_aln >= TAM_DOS_GRUPOS) //Saida bem sucedida caso a condição (1) seja cumprida
             {
-                sem_wait(&monitores_em_sala);	printf("[Monitor %d saiu da sala]\n", id_monitor);
+                printf("[Monitor %d saiu da sala]\n", id_monitor);	sem_wait(&monitores_em_sala);	
                 break;
             }
             else printf("[Monitor %d tentou sair, mas há muitos alunos]\n", id_monitor);
         }
     }
-    //Testar se o professor já pode fechar a sala
     sem_getvalue(&monitores_em_sala, &aux_mtr);
-    if(aux_mtr == 0)
-	sem_post(&sala_vazia);
+	if(aux_mtr == 0) sem_post(&sala_vazia);	//Testa se o professor já pode fechar a sala
 
     pthread_exit(NULL);
 }
@@ -83,12 +73,12 @@ void* Estudantes_Monitores(void* id)
 
 void* Professor_Campiolo(void*)
 {
-    sem_init(&sala_vazia, 0, 0);	printf("[Professor Campiolo abriu a sala]\n");	sleep(3);
+    sem_init(&sala_vazia, 0, 0);    printf("[Professor Campiolo abriu a sala]\n");      sleep(3);
 
-    // Avisar alunos...
-    // Avisar monitores...
+    //Aviso...
 
-    sem_wait(&sala_vazia);			printf("[Professor Campiolo fechou a sala]\n");
+    sem_wait(&sala_vazia);          printf("[Professor Campiolo fechou a sala]\n");
+    
     pthread_exit(NULL);
 }
 
@@ -100,19 +90,23 @@ int main(void)
     sem_init(&vagas_na_sala, 0, 0);
     sem_init(&monitores_em_sala, 0, 0);
 
-    pthread_create(&professor, NULL, Professor_Campiolo, NULL);    
+    pthread_create(&professor, NULL, Professor_Campiolo, NULL);
 
     for(int i = 0; i < QTT_MONITORES; i++)
     {
-        pthread_create(&monitores[i], NULL, Estudantes_Monitores, &i);
+        pthread_create(&(monitores[i]), NULL, Estudantes_Monitores, &i);    sleep(1);
     }
 
     for(int j = 0; j < QTT_ALUNOS; j++)
     {
-        pthread_create(&(alunos[j]), NULL, Alunos_de_SO, &j);
+        pthread_create(&(alunos[j]), NULL, Alunos_de_SO, &j);   sleep(1);
     }
 
+
 	pthread_join(professor, NULL);
+
+    sem_destroy(&vagas_na_sala);    
+    sem_destroy(&monitores_em_sala);
 
     return 0;
 }
